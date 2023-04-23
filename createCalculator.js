a2b198227f9dd39fd2544355a58b87f475b2e88b
heidicb1 <131500888+heidicb1@users.noreply.github.com>
2023-04-22 21:20:51 -0600

    Update createCalculator.js

--- createCalculator.js
+++ createCalculator.js
@@ -6,6 +6,15 @@ let calculator = {
     mul() {
         return this.a * this.b;
     },
+    
+    divide() {
+        return this.a / this.b;
+    },
+
+    sub() {
+        return this.a - this.b;
+    },
+
     read() {
         this.a = +prompt('a?', 0);
         this.b = +prompt('b?', 0);
@@ -15,3 +24,5 @@ let calculator = {
 calculator.read ();
 alert( calculator.sum());
 alert( calculator.mul());
+alert( calculator.divide());
+alert( calculator.sub());
